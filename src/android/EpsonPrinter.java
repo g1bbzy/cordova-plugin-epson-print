e389ff84f6482f9b29b0977053907ffdf898827e
daniel gibbs <danielgibbs4@gmail.com>
2017-03-09 10:02:13 +1300

    Signed-off-by: daniel gibbs <danielgibbs4@gmail.com>

--- src/android/EpsonPrinter.java
+++ src/android/EpsonPrinter.java
@@ -170,10 +170,10 @@ public class EpsonPrinter implements ReceiveListener {
 
         Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
-        // scale image down to 75%
-        //int w = Math.round(((float)image.getWidth() * (float)0.40));
-        //int h = Math.round(((float)image.getHeight() * (float)0.40));
-        //image = Bitmap.createScaledBitmap(image, w, h, false);
+        scale image down to 75%
+        int w = Math.round(((float)image.getWidth() * (float)0.75));
+        int h = Math.round(((float)image.getHeight() * (float)0.75));
+        image = Bitmap.createScaledBitmap(image, w, h, false);
 
         if (mPrinter == null) {
             return false;
